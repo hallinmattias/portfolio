056024ad467623c6962e4d98511f10c99d657060
Mattias <mattiashallin93@gmail.com>
2020-09-25 23:55:25 +0200

    Added favicon

---
+++ components/pageHead.tsx
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+
+export default function PageHead() {
+  return (
+    <Head>
+      <title>Mattias Hallin</title>
+    </Head>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 
-
-import Nav from '../components/nav'
+import PageHead from '../components/pageHead'
 
 export default function IndexPage() {
   return (
     <div>
-      <Nav />
+      <PageHead />
+      
+      <div className="w-full bg-grey-300 border-8 border-black">
+        <h1>Hello World!</h1>
+      </div>
       <div className="py-20">
         <h1 className="text-5xl text-center text-accent-1">
           Mattias Hallin
