796fc41cfe878f3db1ed683f7dcc34b62390ac2a
Mattias <mattiashallin93@gmail.com>
2020-11-15 19:35:34 +0100

    deleted more files

--- pages/spotify.tsx
+++ pages/spotify.tsx
@@ -1,7 +1,5 @@
 import Link from "next/link";
 import PageHead from "../components/PageHead";
-import IndexHeader from "../components/IndexHeader";
-import IndexProjects from "../components/IndexProjects";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function SpotifyPage() {
